c85266c32f59d734713b56a0254ff9f6aae9a255
ArmandoRdzGz <A01254249@itesm.mx>
2021-10-15 18:17:52 +0000

    examen_02_int

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,8 +1,28 @@
 
-
 def main():
+    matriz = crearmatriz()
+    l_col = []
+    if len(matriz) > 0:
+        for i in range(len(matriz[0])):
+            a = 0
+            for j in range(len(matriz)):
+                a += matriz[j][i] 
+            l_col.append(a)
+        print(l_col)
 
-
+def crearmatriz():
+    lista = []
+    filas = int(input())
+    columnas = int(input())
+    if filas > 0 and columnas > 0: 
+        for i in range(filas):
+            lista.append([])
+            for j in range(columnas):
+                n = int(input())
+                lista[i].append(n)
+    else: 
+        print('Error')
+    return lista
 
 if __name__=='__main__':
     main()
